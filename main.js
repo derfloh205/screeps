785aba12de8672f6a097dcc42a3858957fe4a4af
derfloh205 <derfloh205@gmail.com>
2020-03-28 00:31:24 +0100

    auto extension build auto maxCreep spawning

--- main.js
+++ main.js
@@ -2,22 +2,53 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
-const MAX_HARVESTER = 5;
-const MAX_UPGRADER = 2;
+const MAX_EXTENSIONS = 5;
+
+const MAX_HARVESTER = 3;
+const MAX_UPGRADER = 3;
 const MAX_BUILDER = 2;
+
+const MAX_CREEP_LEVEL = 5;
+
+function showSpawnMessage(text) {
+    Game.spawns["Spawn1"].room.visual.text(text, Game.spawns["Spawn1"].pos.x, Game.spawns["Spawn1"].pos.y - 2, {size:'0.5', align: 'left', opacity: 0.8, 'backgroundColor': '#A3E4D7', color:'black'});
+}
+
+function getMaxAvailableCreep() {
+    let lastWorkingConfig = [];
+    
+    for(let i = 0; i < MAX_CREEP_LEVEL; i++) {
+        // dryrun spawnCreep and take highest possible
+        let tryConfig = lastWorkingConfig.slice(0);
+        tryConfig.push(WORK);
+        tryConfig.push(CARRY);
+        tryConfig.push(MOVE);
+        
+        if (Game.spawns["Spawn1"].spawnCreep(tryConfig, "dummy", {dryRun: true}) !== OK) {
+            console.log("not enough energy for " + tryConfig.length + " bodyparts");
+            break;
+        }
+        // push the possible config up
+        lastWorkingConfig = tryConfig.slice(0);
+    }
+    console.log("body parts: " + lastWorkingConfig.length);
+    showSpawnMessage("🛠️ spawning creep with " + lastWorkingConfig.length + " body parts 🛠️");
+    return lastWorkingConfig;
+}
 
 function autoSpawn() {
     let harvesterCreepsNumber = _.filter(Game.creeps, (creep) => creep.memory.role === "harvester").length;
     let upgraderCreepsNumber = _.filter(Game.creeps, (creep) => creep.memory.role === "upgrader").length;
     let builderCreepsNumber = _.filter(Game.creeps, (creep) => creep.memory.role === "builder").length;
     if (harvesterCreepsNumber < MAX_HARVESTER) {
-       Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], "harvester_" + Game.time, {memory: {role: "harvester"}}); 
+        console.log("spawn Harvester");
+       Game.spawns["Spawn1"].spawnCreep(getMaxAvailableCreep(), "harvester_" + Game.time, {memory: {role: "harvester"}}); 
     }
     if (upgraderCreepsNumber < MAX_UPGRADER) {
-       Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], "upgrader_" + Game.time, {memory: {role: "upgrader"}}); 
+       Game.spawns["Spawn1"].spawnCreep(getMaxAvailableCreep(), "upgrader_" + Game.time, {memory: {role: "upgrader"}}); 
     }
     if (builderCreepsNumber < MAX_BUILDER) {
-       Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], "builder_" + Game.time, {memory: {role: "builder"}}); 
+       Game.spawns["Spawn1"].spawnCreep(getMaxAvailableCreep(), "builder_" + Game.time, {memory: {role: "builder"}}); 
     }
 }
 
@@ -31,11 +62,34 @@ function cleanMemory(){
     }
 }
 
+function autoExtension() {
+    var numExtensions = _.filter(Game.structures, (structure) => structure.structureType === STRUCTURE_EXTENSION).length;
+    // try to build extension around the spawn 
+    if(numExtensions < MAX_EXTENSIONS) {
+        let spawn_pos_x = Game.spawns["Spawn1"].pos.x;
+        let spawn_pos_y = Game.spawns["Spawn1"].pos.y;
+        let roomName = Game.spawns["Spawn1"].room.name;
+        
+        for(let rel_x of [-1, 0, +1]) {
+            for(let rel_y of [-1, 0, +1]) {
+                if(rel_x === 0 && rel_y === 0) {
+                    continue;
+                }
+                //console.log("build at " + (spawn_pos_x + rel_x) + "/" + (spawn_pos_x + rel_y) + " room: " + roomName);
+                let build_pos = new RoomPosition(spawn_pos_x + rel_x, spawn_pos_y + rel_y, roomName);
+                build_pos.createConstructionSite(STRUCTURE_EXTENSION, "extension_" + Game.time);
+            }
+        }
+    }
+}
+
 
 module.exports.loop = function () {
     
     cleanMemory();
     autoSpawn();
+    autoExtension();
+    Game.spawns["Spawn1"].room.visual.clear();
 
     var tower = Game.getObjectById('a658142523073561a948107a');
     if(tower) {
